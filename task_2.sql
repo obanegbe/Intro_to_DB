93f08e4f883bf335a6b09c666fbabfcf897bd5f4
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-06-29 18:28:03 +0100

    Create Your Magical Tables

--- task_2.sql
+++ task_2.sql
@@ -25,14 +25,14 @@ CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Order_details (
+CREATE TABLE Order_Details (
     order_detailID INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
