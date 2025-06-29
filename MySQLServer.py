322f8e20f33e3f5fbcf51e077584903708965809
Oluwatobi Obanegbe <emmatobig@gmail.com>
2025-06-29 18:12:59 +0100

    Add Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -18,8 +18,8 @@ def create_database():
 
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
-        print(f"Error connecting to MySQL server: {e}")
+    except mysql.connector.Error:
+        print(f"Error connecting to MySQL server")
 
     finally:
         # Clean up: close cursor and connection
